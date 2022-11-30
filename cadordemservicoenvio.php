3c12308a2f3d063abcf5ee0841a9b02fa9cb6295
GiMJarno <giovanemjarno@gmail.com>
2022-11-30 16:52:20 -0300

    correção envio cadastro ordens de serviço

--- cadordemservicoenvio.php
+++ cadordemservicoenvio.php
@@ -1,12 +1,12 @@
-<html lang="pt-br">
+-<html lang="pt-br">
     <head>
         <script type="text/javascript"></script>
         <title>Envio</title>
     </head>
     <body>
         <?php
-            $post=implode(",",$_POST);
-            echo $post;
+            // $post=implode(",",$_POST);
+            // echo $post;
             $cliente=$_POST['cliente'];
             $disp=$_POST['dispositivo'];
             $serv=$_POST['servico'];
@@ -14,6 +14,8 @@
             require('conexao.php');
             $inserir="insert into ordemserv values('','$cliente', '$disp', '$serv', '$dcc')";
             mysqli_query($db,$inserir) or die ('Não foi possivel cadastrar');
+            echo "<script>alert('Cadastro realizado com sucesso!');
+            window.location.href='cadordemservico.php'</script>";
         ?>
     </body>
 </html>
